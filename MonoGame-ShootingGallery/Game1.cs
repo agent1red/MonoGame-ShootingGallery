e8515399c1556acc723da4be7cab9385d697f83f
Kevin Hudson <khudson@midfinance.com>
2023-10-06 20:39:54 -0400

    Added target, countdown, score to game

--- MonoGame-ShootingGallery/Game1.cs
+++ MonoGame-ShootingGallery/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
 
 namespace MonoGame_ShootingGallery
 {
@@ -9,7 +10,28 @@ namespace MonoGame_ShootingGallery
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
-        public Game1()
+        // Declare content variables 
+
+        Texture2D targetSprite;
+        Texture2D crosshairsSprite;
+        Texture2D backgroundSprite;
+        SpriteFont gameFont;
+
+        static Random r = new Random();
+        static int minValue = 20;
+        static int maxValue = 400; 
+
+        Vector2 targetPosition = new Vector2(r.Next(minValue, maxValue), r.Next(minValue, maxValue));
+        const int targetRadius = 45;
+        MouseState mState;
+        bool mReleased = true;
+        int score = 0;
+        double timer = 10;
+
+
+
+
+        public Game1() 
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
@@ -28,6 +50,11 @@ namespace MonoGame_ShootingGallery
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+
+            targetSprite = Content.Load<Texture2D>("target");
+            crosshairsSprite = Content.Load<Texture2D>("crosshairs");
+            backgroundSprite = Content.Load<Texture2D>("sky");
+            gameFont = Content.Load<SpriteFont>("galleryFont");
         }
 
         protected override void Update(GameTime gameTime)
@@ -35,7 +62,49 @@ namespace MonoGame_ShootingGallery
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
+            // TODO: Add your update logic here. Every single frame this will be updated. 
+
+            //*** Timer settings ***//
+
+            
+
+            if (timer > 0)
+            {
+                timer -= gameTime.ElapsedGameTime.TotalSeconds;
+            }
+
+            if (timer < 0)
+            {
+                timer = 0;
+            }
+            //*** Timer end ***//
+
+
+            //*** Mouse states used for game ***//
+            mState = Mouse.GetState();
+
+            if (mState.LeftButton == ButtonState.Pressed && mReleased == true)
+            {
+                float mouseTargetDistance = Vector2.Distance(targetPosition, mState.Position.ToVector2());
+                if (mouseTargetDistance < targetRadius && timer > 0)
+                {
+                    score++;
+                    targetPosition.X = r.Next(0, _graphics.PreferredBackBufferWidth);
+                    targetPosition.Y = r.Next(0, _graphics.PreferredBackBufferHeight);
+                }
+             
+                mReleased = false;
+            }
+
+            if(mState.LeftButton == ButtonState.Released)
+            {
+                mReleased = true;
+            }
+
+            //*** Mouse states end ***//
+
+
+
 
             base.Update(gameTime);
         }
@@ -46,7 +115,20 @@ namespace MonoGame_ShootingGallery
 
             // TODO: Add your drawing code here
 
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(backgroundSprite, new Vector2(0, 0), Color.White);
+            if (timer > 0)
+            {
+                _spriteBatch.Draw(targetSprite, new Vector2(targetPosition.X - targetRadius, targetPosition.Y - targetRadius), Color.White);
+            }
+            
+            _spriteBatch.DrawString(gameFont, $"The Score: {score}", new Vector2(100,0), Color.White);
+            _spriteBatch.DrawString(gameFont, $"Timer: {Math.Ceiling(timer)}", new Vector2(350,0), Color.White);
+            _spriteBatch.End();
+
             base.Draw(gameTime);
         }
+
+
     }
 }
