f3de11911334bf6fed346079b9c9a00c1490baa4
Kevin Hudson <khudson@midfinance.com>
2023-10-07 22:51:46 -0400

    Added play button and finished game

--- MonoGame-ShootingGallery/Game1.cs
+++ MonoGame-ShootingGallery/Game1.cs
@@ -15,6 +15,7 @@ namespace MonoGame_ShootingGallery
         Texture2D targetSprite;
         Texture2D crosshairsSprite;
         Texture2D backgroundSprite;
+        Texture2D restartButtonSprite;
         SpriteFont gameFont;
 
         static Random r = new Random();
@@ -22,11 +23,15 @@ namespace MonoGame_ShootingGallery
         static int maxValue = 400; 
 
         Vector2 targetPosition = new Vector2(r.Next(minValue, maxValue), r.Next(minValue, maxValue));
+
+        Vector2 startButtonPosition; 
         const int targetRadius = 45;
         MouseState mState;
         bool mReleased = true;
         int score = 0;
         double timer = 10;
+        bool gameEnd = false;
+        int clickCount; 
 
 
 
@@ -35,7 +40,7 @@ namespace MonoGame_ShootingGallery
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
-            IsMouseVisible = true;
+            IsMouseVisible = false;
         }
 
         protected override void Initialize()
@@ -54,11 +59,16 @@ namespace MonoGame_ShootingGallery
             targetSprite = Content.Load<Texture2D>("target");
             crosshairsSprite = Content.Load<Texture2D>("crosshairs");
             backgroundSprite = Content.Load<Texture2D>("sky");
+            restartButtonSprite = Content.Load<Texture2D>("playbutton");
             gameFont = Content.Load<SpriteFont>("galleryFont");
         }
 
         protected override void Update(GameTime gameTime)
         {
+
+            startButtonPosition.X = (_graphics.PreferredBackBufferWidth / 2) - 100;
+            startButtonPosition.Y = (_graphics.PreferredBackBufferHeight / 2) + targetRadius;
+
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
@@ -86,13 +96,27 @@ namespace MonoGame_ShootingGallery
             if (mState.LeftButton == ButtonState.Pressed && mReleased == true)
             {
                 float mouseTargetDistance = Vector2.Distance(targetPosition, mState.Position.ToVector2());
+                float distanceToRestartButton = Vector2.Distance(startButtonPosition, mState.Position.ToVector2());
                 if (mouseTargetDistance < targetRadius && timer > 0)
                 {
                     score++;
+                    clickCount++;
+
+                    if (clickCount == 3)
+                    {
+                        timer += 3;
+                        clickCount = 0;  
+                    }
+
                     targetPosition.X = r.Next(0, _graphics.PreferredBackBufferWidth);
                     targetPosition.Y = r.Next(0, _graphics.PreferredBackBufferHeight);
                 }
-             
+
+                if (gameEnd && distanceToRestartButton < 45) // using 45 as a radius for simplicity, adjust as needed.
+                {
+                    RestartGame();
+                }
+
                 mReleased = false;
             }
 
@@ -103,9 +127,6 @@ namespace MonoGame_ShootingGallery
 
             //*** Mouse states end ***//
 
-
-
-
             base.Update(gameTime);
         }
 
@@ -115,20 +136,41 @@ namespace MonoGame_ShootingGallery
 
             // TODO: Add your drawing code here
 
+            startButtonPosition.X = (_graphics.PreferredBackBufferWidth / 2) - 100; 
+            startButtonPosition.Y = (_graphics.PreferredBackBufferHeight / 2) + targetRadius; 
+
             _spriteBatch.Begin();
             _spriteBatch.Draw(backgroundSprite, new Vector2(0, 0), Color.White);
             if (timer > 0)
             {
                 _spriteBatch.Draw(targetSprite, new Vector2(targetPosition.X - targetRadius, targetPosition.Y - targetRadius), Color.White);
+                _spriteBatch.DrawString(gameFont, $"Target hits: {score}", new Vector2(100, 0), Color.White);
+                _spriteBatch.DrawString(gameFont, $"Time Left: {Math.Ceiling(timer)}", new Vector2(350, 0), Color.White);
+            }
+            else
+            {
+                gameEnd = true; 
+                _spriteBatch.DrawString(gameFont, $"Game Over, Your New Score is: {score}", new Vector2((_graphics.PreferredBackBufferWidth / 2) - 250, _graphics.PreferredBackBufferHeight / 2), Color.White);
+                _spriteBatch.Draw(restartButtonSprite, new Rectangle((int)startButtonPosition.X, (int)startButtonPosition.Y, 90, 50),  Color.Red);
             }
             
-            _spriteBatch.DrawString(gameFont, $"The Score: {score}", new Vector2(100,0), Color.White);
-            _spriteBatch.DrawString(gameFont, $"Timer: {Math.Ceiling(timer)}", new Vector2(350,0), Color.White);
+
+            _spriteBatch.Draw(crosshairsSprite, new Vector2(mState.X-25, mState.Y-25), Color.White);
+            
+            
             _spriteBatch.End();
 
             base.Draw(gameTime);
         }
 
+        private void RestartGame()
+        {
+            score = 0;
+            clickCount = 0;
+            timer = 10;
+            gameEnd = false;
+            targetPosition = new Vector2(r.Next(minValue, maxValue), r.Next(minValue, maxValue));
+        }
 
     }
 }
